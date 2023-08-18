b77b1f6db3cebf9182a474437f1edb0c7948d5f3
Invisibleglass <122887946+Invisibleglass@users.noreply.github.com>
2023-08-18 07:14:37 -0600

    Player Controls created
    player is able to move up and down by holding on the screen

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 swipeStartPos;
+    private PlayerControls controls;
+    private Rigidbody2D rb;
+
+    private float continuousForce = 2f;
+    private bool isTapPressed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        controls = new PlayerControls();
+        controls.Enable();
+    }
+
+    private void OnEnable()
+    {
+        controls.Tap.GoUp.performed += OnHold;
+        controls.Tap.GoUp.canceled += OnHoldEnd;
+        //controls.Swipe.VehicleTeleport.started += OnSwipeStart;
+        //controls.Swipe.VehicleTeleport.canceled += OnSwipeCanceled;
+    }
+
+    private void OnDisable()
+    {
+        controls.Tap.GoUp.performed -= OnHold;
+        controls.Tap.GoUp.canceled -= OnHoldEnd;
+        //controls.Swipe.VehicleTeleport.started -= OnSwipeStart;
+        //controls.Swipe.VehicleTeleport.canceled -= OnSwipeCanceled;
+    }
+
+    private void OnHoldEnd(InputAction.CallbackContext context)
+    {
+        Debug.Log("Tap ended!");
+        isTapPressed = false;
+    }
+
+    private void OnHold(InputAction.CallbackContext context)
+    {
+        Debug.Log("Tap detected!");
+        // Handle tap input
+        AccelerateUp();
+    }
+
+    
+    private void AccelerateUp()
+    {
+        isTapPressed = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isTapPressed == true)
+        {
+            rb.AddForce(Vector2.up * continuousForce, ForceMode2D.Force);
+        }
+    }
+}
